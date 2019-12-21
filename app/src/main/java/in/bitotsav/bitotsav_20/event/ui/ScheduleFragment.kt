b4c0e6a8fb265f8416045fa1d9ef0eb9b7b0effd
RahulKraken <r.rahul4511@gmail.com>
2019-12-21 19:19:03 +0530

    schedule view pager setup with day fragments

--- app/src/main/java/in/bitotsav/bitotsav_20/event/ui/ScheduleFragment.kt
+++ app/src/main/java/in/bitotsav/bitotsav_20/event/ui/ScheduleFragment.kt
@@ -1,12 +1,21 @@
 package `in`.bitotsav.bitotsav_20.event.ui
 
 
+import `in`.bitotsav.bitotsav_20.BitotsavFragment
 import `in`.bitotsav.bitotsav_20.R
+import `in`.bitotsav.bitotsav_20.event.ui.days.DayOneFragment
+import `in`.bitotsav.bitotsav_20.event.ui.days.DayThreeFragment
+import `in`.bitotsav.bitotsav_20.event.ui.days.DayTwoFragment
+import `in`.bitotsav.bitotsav_20.feed.ui.FeedFragment
+import `in`.bitotsav.bitotsav_20.leaderboard.ui.LeaderboardFragment
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import kotlinx.android.synthetic.main.fragment_schedule.*
 
 /**
  * A simple [Fragment] subclass.
@@ -21,5 +30,32 @@ class ScheduleFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_schedule, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        schedule_view_pager.adapter = DayPagerAdapter(activity!!.supportFragmentManager)
+    }
 
+    class DayPagerAdapter(fragmentManager: FragmentManager): FragmentPagerAdapter(fragmentManager) {
+        override fun getItem(position: Int): Fragment {
+            return when(position) {
+                0 -> DayOneFragment()
+                1 -> DayTwoFragment()
+                2 -> DayThreeFragment()
+                else -> DayOneFragment()
+            }
+        }
+
+        override fun getCount(): Int {
+            return 3
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return when(position) {
+                0 -> "DAY 1"
+                1 -> "DAY 2"
+                2 -> "DAY 3"
+                else -> "DAY 1"
+            }
+        }
+    }
 }
