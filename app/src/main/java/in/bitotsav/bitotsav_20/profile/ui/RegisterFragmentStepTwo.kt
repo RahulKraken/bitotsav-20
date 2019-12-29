6ff3f255b72f5f30fb87c52df4cdf2954fc362a2
RahulKraken <r.rahul4511@gmail.com>
2019-12-29 09:20:03 +0530

    register step two to profile

--- app/src/main/java/in/bitotsav/bitotsav_20/profile/ui/RegisterFragmentStepTwo.kt
+++ app/src/main/java/in/bitotsav/bitotsav_20/profile/ui/RegisterFragmentStepTwo.kt
@@ -8,9 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 
 import `in`.bitotsav.bitotsav_20.R
+import `in`.bitotsav.bitotsav_20.profile.data.User
+import `in`.bitotsav.bitotsav_20.utils.SharedPrefUtils
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.fragment_register_fragment_step_two.*
 
 /**
  * A simple [Fragment] subclass.
@@ -18,6 +21,9 @@ import com.google.android.material.button.MaterialButton
 class RegisterFragmentStepTwo : Fragment(), View.OnClickListener {
 
     lateinit var navController: NavController
+
+    lateinit var email: String
+    lateinit var token: String
 
     companion object {
         fun newInstance() = RegisterFragmentStepTwo()
@@ -28,7 +34,7 @@ class RegisterFragmentStepTwo : Fragment(), View.OnClickListener {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val email = arguments?.get("email")
+        email = arguments?.get("email") as String
         println("argument received step two $email")
         return inflater.inflate(R.layout.fragment_register_fragment_step_two, container, false)
     }
@@ -41,7 +47,27 @@ class RegisterFragmentStepTwo : Fragment(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         when (v!!.id) {
-            R.id.verify_btn -> navController.navigate(R.id.action_registerFragmentStepTwo_to_profileFragment)
+            R.id.verify_btn -> tryToVerify()
         }
+    }
+
+    private fun tryToVerify() {
+        val name = register_name.text.toString()
+        val gender = register_gender.text.toString()
+        val phone = register_phone.text.toString()
+        val clgName = register_clg_name.text.toString()
+        val clgCity = register_clg_city.text.toString()
+        val clgState = register_clg_state.text.toString()
+        val clgId = register_clg_id.text.toString()
+        val otp = register_otp.text.toString()
+
+        // TODO: Check for not empty fields
+
+        SharedPrefUtils(context!!).setUser(User(93, name, email, phone, 54, clgName, clgCity, clgState, clgId, true, "lkds", 45))
+        navigateToProfile()
+    }
+
+    private fun navigateToProfile() {
+        navController.navigate(R.id.action_registerFragmentStepTwo_to_profileFragment)
     }
 }
