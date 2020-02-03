4ded24238fc9475d488199dd1b9a5970561b25e9
RahulKraken <r.rahul4511@gmail.com>
2020-02-04 01:12:32 +0530

    populate details and placeholder messages in event detail

--- app/src/main/java/in/bitotsav/bitotsav_20/schedule/ui/EventDetailActivity.kt
+++ app/src/main/java/in/bitotsav/bitotsav_20/schedule/ui/EventDetailActivity.kt
@@ -15,19 +15,54 @@ class EventDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_event_detail)
 
         val event = intent.getSerializableExtra(resources.getString(R.string.event_intent_pass_key)) as Event
-        println(event)
 
         bindData(event)
     }
 
-    // TODO: bind more views
     private fun bindData(event: Event) {
+        // text details
         event_detail_title.text = event.name
-//        event_detail_rules.text = event.description
+        event_detail_desc.text = event.description
+        event_detail_rules.text = event.rulesAndRegulations
         event_detail_day_time.text = "Day ${event.day}, ${event.timing}"
         event_detail_venue.text = event.venue
 
+        // points
+        val points = event.points?.split("-")
+        if (!points.isNullOrEmpty() && points.size > 1) {
+            if (points[0].isNotEmpty()) prize_one.text = points[0]
+            if (points[1].isNotEmpty()) prize_two.text = points[1]
+            if (points[2].isNotEmpty()) prize_three.text = points[2]
+        } else {
+            event_detail_prizes_label.visibility = View.GONE
+            event_detail_prizes_container.visibility = View.GONE
+        }
+
+        // TODO: get winners, format then parse and display
+        // for now it's not visible
+        event_detail_winner_container.visibility = View.GONE
+
+        // coordinators
+        val coordinators = event.contactInformation?.split("\n")
+        if (!coordinators.isNullOrEmpty() && coordinators[0].isNotEmpty()) {
+            val v = getNameAndPhone(coordinators[0])
+            coordinator_one_label.text = v.first
+            coordinator_one.text = v.second
+        }
+        if (!coordinators.isNullOrEmpty() && coordinators[1].isNotEmpty()) {
+            val v = getNameAndPhone(coordinators[1])
+            coordinator_two_label.text = v.first
+            coordinator_two.text = v.second
+        }
+
         populateRecyclerView(event)
+    }
+
+    private fun getNameAndPhone(s: String): Pair<String, String> {
+        val name = s.split(":")[0].split(".")[1].trim()
+        val phone = s.split(":")[1].trim()
+        println("name $name, phone $phone")
+        return Pair(name, phone)
     }
 
     private fun populateRecyclerView(event: Event) {
