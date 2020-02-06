715b9de6e5cc0d2a092cae6da6971b042c1817ff
RahulKraken <r.rahul4511@gmail.com>
2020-02-07 01:34:47 +0530

    splash screen finished

--- app/src/main/java/in/bitotsav/bitotsav_20/SplashScreen.kt
+++ app/src/main/java/in/bitotsav/bitotsav_20/SplashScreen.kt
@@ -2,15 +2,22 @@ package `in`.bitotsav.bitotsav_20
 
 import `in`.bitotsav.bitotsav_20.activities.MainActivity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
 
 class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.md_red_900)
 
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
