bf65b9ecff1e8e4c500c581fe0b3731ababba34e
RahulKraken <r.rahul4511@gmail.com>
2020-02-09 13:35:24 +0530

    contact us activity

--- app/src/main/java/in/bitotsav/bitotsav_20/activities/ContactUsActivity.kt
+++ app/src/main/java/in/bitotsav/bitotsav_20/activities/ContactUsActivity.kt
@@ -1,13 +1,31 @@
 package `in`.bitotsav.bitotsav_20.activities
 
 import `in`.bitotsav.bitotsav_20.R
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_contact_us.*
 
-class ContactUsActivity : AppCompatActivity() {
+class ContactUsActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_us)
+
+        email.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.email -> {
+                val intent = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "bitotsav@bitmesra.ac.in", null))
+                startActivity(Intent.createChooser(intent, "Send email..."))
+            }
+            R.id.container_contact_one -> {}
+            R.id.container_contact_two -> {}
+            else -> println("not clicked anything!")
+        }
     }
 }
