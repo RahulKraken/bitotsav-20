5be55df3d971f32bb21a94ee07a87269a1b4f2a6
RahulKraken <r.rahul4511@gmail.com>
2020-02-13 15:16:44 +0530

    database update

--- app/src/main/java/in/bitotsav/bitotsav_20/db/AppDatabase.kt
+++ app/src/main/java/in/bitotsav/bitotsav_20/db/AppDatabase.kt
@@ -19,7 +19,7 @@ import kotlinx.coroutines.launch
 
 @Database(
     entities = [Event::class, Winner::class, Feed::class],
-    version = 4,
+    version = 5,
     exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
